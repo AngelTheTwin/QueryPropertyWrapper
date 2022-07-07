fd13bddc68d5245a4709514a0659f7dbf15c58f4
Angel de Jesús Sánchez Morales <angeldejesussanchezm@icloud.com>
2022-07-07 00:52:20 -0500

    Initial Commit

---
+++ Sources/QueryPropertyWrapper/QueryPropertyWrapper.swift
@@ -0,0 +1,6 @@
+public struct QueryPropertyWrapper {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/QueryPropertyWrapperTests/QueryPropertyWrapperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import QueryPropertyWrapper
+
+final class QueryPropertyWrapperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(QueryPropertyWrapper().text, "Hello, World!")
+    }
+}
